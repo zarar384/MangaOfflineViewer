592bff444b0d9fb4979f060f506298ec1d1f8aae
zarar384 <fel98_98@mail.ru>
2025-11-17 16:09:46 +0100

    integrate PWA support

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+  providers: [provideServiceWorker('ngsw-worker.js', {
+              enabled: !isDevMode(),
+              registrationStrategy: 'registerWhenStable:30000'
+            }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })]
+});
