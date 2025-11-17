6b3dd5c9dc97005f943c4134a95ffdab25597edb
zarar384 <fel98_98@mail.ru>
2025-11-17 21:06:40 +0100

    SW only in prod

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,16 @@ import { AppComponent } from './app/app.component';
 import { isDevMode } from '@angular/core';
 import { provideServiceWorker } from '@angular/service-worker';
 
+const providers = [];
+if (!isDevMode()) {
+  providers.push(
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: true,
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  );
+}
+
 bootstrapApplication(AppComponent, {
-  providers: [provideServiceWorker('ngsw-worker.js', {
-              enabled: !isDevMode(),
-              registrationStrategy: 'registerWhenStable:30000'
-            }), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          })]
+  providers
 });
